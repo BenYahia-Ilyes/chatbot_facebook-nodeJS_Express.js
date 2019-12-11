d2b4ca4e135565764223279f011d0c56f4e54df1
BenYahia-Ilyes <djerbyilyes@gmail.com>
2019-12-11 22:15:56 +0100

    make it better

--- index.js
+++ index.js
@@ -14,17 +14,14 @@ app.listen(app.get('port'),function(){ console.log('running, port: 5000 ')});
 
 
 // index
-app.get('/', function (req, res) {
+app.get('/',(req, res)  => {
 	res.send('hello world i am a secret bot yoo yo')
 })
 
-app.get('/yoo', function (req, res) {
-	res.send('yoo yo')
-})
 
 // for facebook verification
-app.get('/webhook/', function (req, res) {
-	console.log(req.query)
+app.get('/webhook',(req, res) => {
+	//console.log(req)
 	if (req.query['hub.verify_token'] === 'yoo_yo') {
 		res.send(req.query['hub.challenge'])
 	} else {
@@ -35,41 +32,64 @@ app.get('/webhook/', function (req, res) {
 
 
 const token = "EAAHASNZACqPQBAEqADudZBKZA3ZCJ8MQ7tXmVoED88q2B8R7k5bT4PohiaG6qgbuDffAmqrWxCQWT5tn6HYxiFXjWzc48OZBnJN9ZAwF887A08OgCaTO3nn87OANWy44naGBSOa9lmSGtBbLWhtNnzKCrcCRkxq4yicslj72NjPQZDZD";
-app.post('/webhook/', function(req, res) {
-    var messaging_events = req.body.entry[0].messaging;
-    for (var i = 0; i < messaging_events.length; i++) {
-        var event = req.body.entry[0].messaging[i];
-        var sender = event.sender.id;
-        if (event.message && event.message.text) {
-            var text = event.message.text;
-            sendTextMessage(sender, text + "!");
-        }
-    }
+app.post('/webhook', (req, res) => {
+
+	console.log(req.body)
+	console.log("**********************************************************************************")
+
+	var msg = req.body.entry[0].messaging[0];
+	//console.log(msg)
+	var  sender = msg.sender.id;
+	console.log(msg)
+	if (msg.message) {
+
+		if (msg.message.attachments)
+		{
+			sendText(sender, "Je ne sais pas traiter ce type de demande !");
+		} 
+		else if (msg.message.text)
+		{
+			var text = msg.message.text;
+
+			if (text == "Comment vas-tu ?")
+			{
+				sendText(sender, "special treatment ");
+
+			}
+			else
+			{
+				sendText(sender, text);
+
+			}	
+		}
+
+
+
+	}
+
     res.sendStatus(200);
 });
-function sendTextMessage(sender, text) {
-    var messageData = {
-        text: text
-    };
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {
-            access_token: token
-        },
+
+
+function sendText(sender, text) {
+    var toSend = {text: text};
+    request(
+	{
+
+        url: 'https://graph.facebook.com/v5.0/me/messages',
+        qs: { access_token: token },
         method: 'POST',
         json: {
-            recipient: {
-                id: sender
-            },
-            message: messageData,
-        }
-    }, function(error, response, body) {
+            recipient: { id: sender },
+            message: toSend,
+		}
+		
+	}, function(error, response, body)
+	 {
         if (error) {
             console.log('Error:', error);
         } else if (response.body.error) {
             console.log('Error: ', response.body.error);
         }
-    });
+     });
 }
-
-
